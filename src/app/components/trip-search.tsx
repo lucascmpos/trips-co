eef3d3f9522eb9a6b1cc0c52f77e2c3cd0ba4cda
lucascmpos <lucasoliveiracampos81@gmail.com>
2024-07-01 19:14:59 -0300

    feat: add search bg search-trip

--- src/app/components/trip-search.tsx
+++ src/app/components/trip-search.tsx
@@ -1,11 +1,12 @@
 import CurrencyInput from "@/components/currency-input";
+import SearchButton from "@/components/search-button";
 import { DatePicker } from "@/components/ui/date-picker";
 import Input from "@/components/ui/input";
 import React from "react";
 
 const TripSearch = () => {
   return (
-    <div className="container mx-auto p-5">
+    <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat">
       <h1 className="font-semibold text-2xl text-center text-primaryDarker">
         Encontre sua próxima <span className="text-primary">viagem</span>!
       </h1>
@@ -16,6 +17,8 @@ const TripSearch = () => {
           <DatePicker />
           <CurrencyInput placeholder="Orçamento" />
         </div>
+
+        <SearchButton>Buscar</SearchButton>
       </div>
     </div>
   );
