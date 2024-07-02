16fbb8dc4b98c3199086888e63d67dfec68d436e
lucascmpos <lucasoliveiracampos81@gmail.com>
2024-07-02 13:08:14 -0300

    feat: testing commit

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="h-16 w-16 animate-spin rounded-full border-t-4 border-solid border-[#8162FF]"></div>
+      <div className="h-16 w-16 animate-spin rounded-full border-t-5 border-solid border-[#8162FF]"></div>
     </div>
   );
 };
