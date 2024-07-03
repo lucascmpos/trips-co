5a287c6c0e1b725e79835984fd11456193b2a0c9
lucascmpos <lucasoliveiracampos81@gmail.com>
2024-07-03 14:53:49 -0300

    feat: add trip search page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "./globals.css";
 import { NextAuthProvider } from "@/providers/auth";
-import Header from "@/components/header";
+import "./globals.css";
+import { Poppins } from "next/font/google";
 import { Toaster } from "@/providers/toaster";
+import Header from "@/components/header";
 import Footer from "@/components/footer";
 
 const poppins = Poppins({
@@ -11,24 +10,28 @@ const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800", "900"],
 });
 
-export const metadata: Metadata = {
-  title: "Trips CO",
-  description: "Trips reservation application",
+export const metadata = {
+  title: "FSW Trips",
+  description: "Sistema de Reserva de Viagens TOP!",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={poppins.className}>
         <NextAuthProvider>
-          <Header />
-          <div className="flex-1">{children}</div>
-          <Toaster />
-          <Footer />
+          <div className="flex flex-col h-screen">
+            <div className="h-[94px]">
+              <Header />
+            </div>
+            <div className="flex-1 ">{children}</div>
+
+            <Footer />
+          </div>
         </NextAuthProvider>
       </body>
     </html>
--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+"use client";
 
-const TripsSearch = () => {
-  return <div>TripsSearch</div>;
+import TripItem from "@/components/trip-item";
+import { Trip } from "@prisma/client";
+import { useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
+
+const Trips = () => {
+  const [trips, setTrips] = React.useState<Trip[]>([]);
+
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const fetchTrips = async () => {
+      const response = await fetch(
+        `http://localhost:3000/api/trips/search?text=${
+          searchParams.get("text") ?? ""
+        }&startDate=${searchParams.get("startDate")}&budget=${searchParams.get(
+          "budget"
+        )}`
+      );
+
+      const data = await response.json();
+
+      setTrips(data);
+    };
+
+    fetchTrips();
+  }, []);
+
+  return (
+    <div className="container mx-auto flex flex-col items-center lg:items-start p-5 lg:pt-10">
+      <h1 className="text-primaryDarker font-semibold text-xl lg:w-full lg:text-left lg:text-[2.5rem]">
+        Viagens encontradas
+      </h1>
+      <h2 className="text-grayPrimary font-medium mb-5 lg:mt-6 lg:w-full lg:text-left">
+        {trips.length > 0
+          ? "Listamos as melhores viagens pra você!"
+          : "Não encontramos nada nos seus parâmetros! "}
+      </h2>
+
+      <div className="flex flex-col gap-4 lg:grid lg:grid-cols-4 lg:gap-10 lg:mt-6 lg:pb-16">
+        {trips?.map((trip) => (
+          <TripItem key={trip.id} trip={trip} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default TripsSearch;
+export default Trips;
