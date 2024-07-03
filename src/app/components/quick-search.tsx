7f43f537e587aaef921ab6aaea59b06a9d5f5e65
lucascmpos <lucasoliveiracampos81@gmail.com>
2024-07-03 15:06:21 -0300

    feat: ensure quick search is working

--- src/app/components/quick-search.tsx
+++ src/app/components/quick-search.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const QuickSearch = () => {
@@ -13,25 +14,37 @@ const QuickSearch = () => {
       </div>
 
       <div className="flex w-full justify-between mt-5 gap-2">
-        <div className="flex flex-col border rounded-lg p-2 w-1/4 cursor-pointer hover:bg-secondary items-center ">
+        <Link
+          href={`/trips/search?text=hotel`}
+          className="flex flex-col border rounded-lg p-2 w-1/4 cursor-pointer hover:bg-secondary items-center "
+        >
           <Image src="/hotel-icon.png" width={32} height={32} alt="Hotel" />
           <p className="text-sm text-grayPrimary  mt-5">Hotel</p>
-        </div>
+        </Link>
 
-        <div className="flex flex-col border rounded-lg p-2 w-1/4 cursor-pointer hover:bg-secondary items-center ">
+        <Link
+          href={`/trips/search?text=fazenda`}
+          className="flex flex-col border rounded-lg p-2 w-1/4 cursor-pointer hover:bg-secondary items-center "
+        >
           <Image src="/farm-icon.png" width={32} height={32} alt="Fazenda" />
           <p className="text-sm text-grayPrimary  mt-5">Fazenda</p>
-        </div>
+        </Link>
 
-        <div className="flex flex-col border rounded-lg p-2 w-1/4 cursor-pointer hover:bg-secondary items-center ">
-          <Image src="/cottage-icon.png" width={32} height={32} alt="Hotel" />
+        <Link
+          href={`/trips/search?text=chalé`}
+          className="flex flex-col border rounded-lg p-2 w-1/4 cursor-pointer hover:bg-secondary items-center"
+        >
+          <Image src="/cottage-icon.png" width={32} height={32} alt="Chale" />
           <p className="text-sm text-grayPrimary  mt-5">Chalé</p>
-        </div>
+        </Link>
 
-        <div className="flex flex-col border rounded-lg p-2 w-1/4 cursor-pointer hover:bg-secondary items-center ">
-          <Image src="/inn-icon.png" width={32} height={32} alt="Hotel" />
+        <Link
+          href={`/trips/search?text=pousada`}
+          className="flex flex-col border rounded-lg p-2 w-1/4 cursor-pointer hover:bg-secondary items-center "
+        >
+          <Image src="/inn-icon.png" width={32} height={32} alt="Pousada" />
           <p className="text-sm  text-grayPrimary  mt-5">Pousada</p>
-        </div>
+        </Link>
       </div>
     </div>
   );
